f5f60f4ca8b2fee50f5a90649c3df103a59e9192
Daniel Mateu <danielmateu86@gmail.com>
2023-03-06 16:43:29 +0100

    Using SSP cleaning the components to the maximum, no useEffects, states... looks really good!

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,30 @@
 import Image from 'next/image'
 import React from 'react'
 
-export const Sidebar = ({podcast}) => {
+export const Sidebar = ({podcastData}) => {
+
+    console.log(podcastData);
 
     return (
         <div className='shadow-lg flex flex-col   text-start rounded-xl p-2 m-2 w-4/12 h-fit'>
-        {podcast && (
+        {podcastData && (
             <>
                 <Image
-                    src={podcast.artworkUrl600}
-                    alt={`${podcast.collectionName} - ${podcast.trackName}`}
+                    src={podcastData.results[0].artworkUrl600}
+                    alt={`${podcastData.results[0].collectionName} - ${podcastData.results[0].trackName}`}
                     width={200}
                     height={200}
                     className='p-6 m-auto'
                 />
                 <hr />
                 <div className='text-start py-4 px-2'>
-                    <p className='font-semibold text-gray-600'>{podcast.collectionName}</p>
-                    <p className='text-gray-400'>by {podcast.artistName}</p>
+                    <p className='font-semibold text-gray-600'>{podcastData.results[0].collectionName}</p>
+                    <p className='text-gray-400'>by {podcastData.results[0].artistName}</p>
                 </div>
                 <hr />
                 <div className='text-start py-4 px-2'>
                     <p className='font-semibold text-gray-600'>Description:</p>
-                    <p className='text-gray-400'>{podcast.genres}</p>
+                    <p className='text-gray-400'>{podcastData.results[0].genres}</p>
                 </div>
             </>
         )}
--- src/pages/episode/[id].js
+++ src/pages/episode/[id].js
@@ -1,90 +1,32 @@
+import { Episode } from '@/components/Episode'
 import { Layout } from '@/components/Layout'
-import { formatearDuracion, formatearFecha } from '@/helpers/helpers'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
 
-const EpisodePage = () => {
-    const router = useRouter()
-    const { id } = router.query
-    const [episodeData, setEpisodeData] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
-
-
-    useEffect(() => {
-        async function fetchEpisodeData() {
-            setIsLoading(true)
-            const storedEpisodeData = localStorage.getItem(`episode-${id}`)
-            const storedTimestamp = localStorage.getItem(`episode-${id}-timestamp`)
-            const timestamp = Date.now()
-            if (storedEpisodeData && storedTimestamp && (timestamp - storedTimestamp < 86400000)) {
-                setEpisodeData(JSON.parse(storedEpisodeData))
-                setIsLoading(false)
-            } else {
-                const response = await fetch(`https://itunes.apple.com/search?term=${id}&entity=podcastEpisode`)
-                const data = await response.json()
-                setEpisodeData(data)
-                localStorage.setItem(`episode-${id}`, JSON.stringify(data))
-                localStorage.setItem(`episode-${id}-timestamp`, timestamp)
-                setIsLoading(false)
-            }
-        }
-        fetchEpisodeData()
-    }, [id])
+const EpisodePage = ({ episodeData }) => {
 
     return (
         <Layout
             title={episodeData && episodeData.results[0].trackName}
             description={episodeData && episodeData.results[0].collectionName}
         >
-            {isLoading && <p>Loading...</p>}
+            {/* {isLoading && <p>Loading...</p>} */}
             {episodeData && (
-                <div className='flex gap-4'>
-                    <div className='flex flex-col gap-4 w-4/12 shadow-lg p-4 h-fit'>
-                        <div className='flex items-center justify-center p-4'>
-                            <Link
-                                href={`/podcast/${episodeData.results[0].collectionId}`}
-                            >
-                                <Image
-                                    src={episodeData.results[0].artworkUrl60}
-                                    alt={episodeData.results[0].collectionName}
-                                    width={150}
-                                    height={150}
-                                    className='hover:animate-pulse'
-
-                                />
-                            </Link>
-                        </div>
-                        <div>
-                            <Link
-                                href={`/podcast/${episodeData.results[0].collectionId}`}
-                                className='font-semibold'>{episodeData.results[0].collectionName}</Link>
-                            {/* Artist */}
-                            <p> by {episodeData.results[0].collectionName}</p>
-                        </div>
-                        <div>
-                            <p className='font-semibold'>Description:</p>
-                            {/* Cut and show the first 20 words */}
-                            <p className='text-justify'>{episodeData.results[0].description.split(' ').slice(0, 10).join(' ')}</p>
-                        </div>
-                        {/* Description */}
-
-                    </div>
-                    <div className='w-8/12 shadow-lg p-6'>
-                        <h2 className='font-semibold'>{episodeData.results[0].trackName}</h2>
-                        <p className='text-justify'>{episodeData.results[0].description}</p>
-
-                        {/* <p>{formatearFecha(episodeData.results[0].releaseDate)}</p> */}
-                        {/* <p>{formatearDuracion(episodeData.results[0].trackTimeMillis)}</p> */}
-                        <audio controls className='w-full my-4'>
-                            <source src={episodeData.results[0].previewUrl} type='audio/mp4' />
-                        </audio>
-                    </div>
-                </div>
+                <Episode episodeData={episodeData} />
             )}
         </Layout>
     )
+}
+
+export const getServerSideProps = async (context) => {
+    const { id } = context.params
+    const response = await fetch(`https://itunes.apple.com/search?term=${id}&entity=podcastEpisode`)
+    const data = await response.json()
+    // console.log(data);
+
+    return {
+        props: {
+            episodeData: data
+        }
+    }
 }
 
 export default EpisodePage
