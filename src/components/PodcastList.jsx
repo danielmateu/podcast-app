ecbeadea6bba2d46e7ab7dbe54d5f42db2bcff4f
Daniel Mateu <danielmateu86@gmail.com>
2023-03-06 14:24:21 +0100

    Fin de la app

--- src/components/PodcastList.jsx
+++ src/components/PodcastList.jsx
@@ -1,16 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import 'animate.css';
 
 export const PodcastList = ({filteredPodcasts,}) => {
     return (
-        <ul className='grid grid-cols-4 gap-6 text-center'>
+        <ul className='grid grid-cols-4 gap-6 text-center '>
             {filteredPodcasts.map((podcast) => (
                 <Link
                     key={podcast.id.attributes['im:id']}
                     href={`/podcast/${podcast.id.attributes['im:id']}`}
                 >
-                    <li className='flex flex-col items-center shadow-xl hover:shadow-md transition-all rounded-xl py-2 px-4 relative my-12'>
+                    <li className='flex flex-col items-center shadow-xl hover:shadow-md transition-all rounded-xl py-2 px-4 relative my-12 '>
                         <Image src={podcast['im:image'][2].label} alt={podcast['im:name'].label} width={100} height={100} className='rounded-full absolute -top-24  hover:scale-110 hover:rotate-12 hover:shadow-lg transition-all hover:cursor-pointer' />
                         <div className='h-24 flex flex-col justify-end'>
                             <h2 className='font-semibold text-gray-700'>{podcast['im:name'].label.toUpperCase().split(' ').slice(0, 2).join(' ')}</h2>
--- src/pages/episode/[id].js
+++ src/pages/episode/[id].js
@@ -1,6 +1,7 @@
 import { Layout } from '@/components/Layout'
 import { formatearDuracion, formatearFecha } from '@/helpers/helpers'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
@@ -42,16 +43,23 @@ const EpisodePage = () => {
                 <div className='flex gap-4'>
                     <div className='flex flex-col gap-4 w-4/12 shadow-lg p-4 h-fit'>
                         <div className='flex items-center justify-center p-4'>
-                            <Image
-                                src={episodeData.results[0].artworkUrl60}
-                                alt={episodeData.results[0].collectionName}
-                                width={150}
-                                height={150}
+                            <Link
+                                href={`/podcast/${episodeData.results[0].collectionId}`}
+                            >
+                                <Image
+                                    src={episodeData.results[0].artworkUrl60}
+                                    alt={episodeData.results[0].collectionName}
+                                    width={150}
+                                    height={150}
+                                    className='hover:animate-pulse'
 
-                            />
+                                />
+                            </Link>
                         </div>
                         <div>
-                            <p className='font-semibold'>{episodeData.results[0].collectionName}</p>
+                            <Link
+                                href={`/podcast/${episodeData.results[0].collectionId}`}
+                                className='font-semibold'>{episodeData.results[0].collectionName}</Link>
                             {/* Artist */}
                             <p> by {episodeData.results[0].collectionName}</p>
                         </div>
