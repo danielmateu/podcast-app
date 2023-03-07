f7e199c9e7819a4c1ae62f3e928db5cb7cd5f89e
Daniel Mateu <danielmateu86@gmail.com>
2023-03-07 12:04:01 +0100

    Tests sobre Filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,7 +4,11 @@ export const Filter = ({ searchTerm, setSearchTerm, filteredPodcasts }) => {
 
     return (
         <div className="flex gap-2 mb-16 justify-end items-center ">
-            <label htmlFor="filter" className='bg-sky-400 px-2 rounded-md text-white font-semibold'>{filteredPodcasts.length}</label>
+            <label
+                data-cy='filter-label'
+                htmlFor="filter"
+                className='bg-sky-400 px-2 rounded-md text-white font-semibold'>{filteredPodcasts.length}
+            </label>
             <input
                 type="text"
                 placeholder='Filter podcasts...'
@@ -12,6 +16,7 @@ export const Filter = ({ searchTerm, setSearchTerm, filteredPodcasts }) => {
                 className='p-2'
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
+                data-cy='filter-input'
             />
         </div>
     )
